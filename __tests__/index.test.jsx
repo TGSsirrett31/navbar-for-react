dbba2cbb02c78f2378b628e9cab5b069f0c1cf6c
TGSsirrett31 <ssirrettcamara@gmail.com>
2024-06-25 00:52:18 -0400

    Fix Jest configuration and add test files

--- __tests__/index.test.jsx
+++ __tests__/index.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import NavBar from './index';
+import NavBar from '../index';
 
 test('renders Navbar component', () => {
     render(<NavBar />);
